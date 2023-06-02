a05ecd3442084a1e70fcd8162262f7b68337674b
Calista Biondic <calistabiondic@gmail.com>
2023-06-02 17:26:08 -0400

    issues with trilinos

--- nnls_mod.h
+++ nnls_mod.h
@@ -1,30 +1,54 @@
 
 #include <iostream>
+#include <Epetra_SerialComm.h>
+#include <Epetra_Map.h>
 #include <Epetra_CrsMatrix.h>
 #include <Epetra_Vector.h>
-#include <eigen/Eigen/Dense>
+#include "eigen-3.4.0/Eigen/Dense"
+
+// template <class Epetra_CrsMatrix> class NNLS_mod{
+// public:
+//     enum {
+//         RowsAtCompileTime = Epetra_CrsMatrix::NumGlobalRows;
+//         ColsAtCompileTime = Epetra_CrsMatrix::NumGlobalCols;
+//     };
+
+//     typedef Matrix<unsigned int, ColsAtCompileTime, 1> IndicesType;
+    
+//     Epetra_SerialComm Comm;
+//     typedef Epetra_Map<ColsAtCompileTime,0,Comm> SolutionMap;
+//     typedef Epetra_CrsMatrix<Epetra_DataAccess::Copy,SolutionMap, 1> SolutionVectorType;
+
+//     NNLS_mod();
+
+//     NNLS_mod(const Epetra_CrsMatrix &A, int max_iter = -1, float eps=1e-10);
+
+//     template <typename Epetra_CrsMatrix> NNLS_mod<Epetra_CrsMatrix> &compute(const Epetra_CrsMatrix &A);
+
+
+//     bool solve(const Epetra_Vector &b);
+
+// protected:
+//     unsigned int _max_iter;
+//     unsigned int _num_ls;
+//     float _epsilon;
+//     Epetra_CrsMatrix _A;
+//     Epetra_Vector _x;
+//     unsigned int _numInactive;
+//     const int _numCols;
+    
+// };
 
 class NNLS_mod{
-public:
-    NNLS_mod(const Epetra_CrsMatrix &A, int max_iter = -1, float eps=1e-10):
-    _max_iter(max_iter), _num_ls(0), _epsilon(eps), _A(A), _x(A.ColMap()), _numInactive(0){}
-
-    bool solve(const Epetra_Vector &b);
-    typedef Eigen::Matrix<int, A.NumGlobalCols(), 1> IndicesType;
-
-protected:
-    unsigned int _max_iter;
-    unsigned int _num_ls;
-    float _epsilon;
-    Epetra_CrsMatrix _A;
-    Epetra_Vector _x;
-    unsigned int _numInactive;
-    IndicesType _index_sets;
+    const Epetra_CrsMatrix A; 
+    unsigned int max_iter;
+    float eps;
+    public:
+        NNLS_mod (const Epetra_CrsMatrix, unsigned int, float);
+        bool solve(const Epetra_Vector &b);
 };
 
 bool NNLS_mod::solve(const Epetra_Vector &b){
-    _num_ls = 0;
-    _index_sets = IndicesType::LinSpaced(_A.NumGlobalCols(), 0, _A.NumGlobalCols()- 1);
-    _numInactive = 0;
-
+    unsigned int _num_ls = 0;
+    return true;
 }
